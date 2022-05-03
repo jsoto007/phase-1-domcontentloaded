5146c847b2fe25fc33d0bea594cc7b2f6a68b690
Jose Soto <jsoto7087@gmail.com>
2022-05-03 11:51:08 -0400

    commit tree 81f45f829c21c690ec7485f0ab8326c39d92d809

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    let p = document.getElementById("text");
+    p.innerHTML = "This is really cool!"
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+--- DOMContentLoaded  event is triggered to safely execute our code. 
+By creating a event listener, we can keep out code from immediately firing 
+when index.js is loaded.
+--- Setting up an event listener for DOMContentLoaded
+-- As always, addEventListener takes a string with the name of the event and a callback function. 
+
+*/
